3ec15c431fac2673b6357baeeb0c56bfaeab4975
Cesar Flores <136465@udlondres.com>
2026-04-04 14:28:38 -0600

    Instalación de Laravel Socialite
    Instalación de paquete de autenticación para proveedores OAuth2

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Socialite;
+
+class AuthController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+
+    public function callback()
+    {
+        $user = Socialite::driver('github')->user();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/auth/redirect', [AuthController::class, 'redirect'])->name('auth.redirect');
+
+Route::get('/auth/callback', [AuthController::class, 'callback'])->name('auth.callback');
