de14f850f8c1d61b1a074bc3276f1d13a5b0767e
Cesar Flores <136465@udlondres.com>
2026-04-06 19:38:02 -0600

    Configuración de autenticación con Facebook
    Implementación de OAuth2 para autenticación con Facebook

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Socialite;
 
@@ -9,12 +10,28 @@ class AuthController extends Controller
 {
     public function redirect()
     {
-        return Socialite::driver('github')->redirect();
+        return Socialite::driver('facebook')->redirect();
     }
 
 
     public function callback()
     {
-        $user = Socialite::driver('github')->user();
+        $userFacebook = Socialite::driver('facebook')->user();
+        // $user = Socialite::driver('facebook')->stateless()->user();
+
+        $user = User::firstOrCreate([
+            'email' => $userFacebook->getEmail(),
+            'provider' => 'Facebook',
+        ], [
+            'name' => $userFacebook->getName(),
+            'password' => '',
+            'provider_id' => $userFacebook->getId(),
+            'avatar' => $userFacebook->getAvatar(),
+            'nick_name' => $userFacebook->getNickname(),
+        ]);
+
+        auth()->login($user);
+
+        return redirect()->to('/dashboard');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,24 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens;
+
+    /** @use HasFactory<UserFactory> */
+    use HasFactory;
+
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +31,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'provider',
+        'provider_id',
+        'avatar',
+        'nick_name',
     ];
 
     /**
@@ -30,6 +45,17 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'profile_photo_url',
     ];
 
     /**
